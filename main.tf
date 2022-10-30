35efa3b0deeb6019ab9c15544d8283aed5e40979
ddubey101 <116831640+ddubey101@users.noreply.github.com>
2022-10-30 12:33:51 +0530

    added user role to access aws

--- main.tf
+++ main.tf
@@ -9,6 +9,9 @@ terraform {
 
 provider "aws" {
   region = var.region
+  role_arn     = "arn:aws:iam::432384616762:role/dev-user-role-101"
+  session_name = "SESSION_NAME"
+  external_id  = "ddubey-dev-user101"
 }
 
 variable "region" {
